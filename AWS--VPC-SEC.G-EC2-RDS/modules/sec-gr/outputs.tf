e8881bb87fb02246d197aee61229e2f9ee04fc64
Vitali <61657613+Vitali-N@users.noreply.github.com>
2022-07-03 23:20:20 +0300

    Delete AWS--VPC-SEC.G-EC2-RDS directory

--- AWS--VPC-SEC.G-EC2-RDS/modules/sec-gr/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id-security-group-Bastion-host" {
-    description = "id security group Bastion host:"
-    value = aws_security_group.bastion_host.id
-}
-
-output "id-security-group-Private-host" {
-    description = "id security group Private host:"
-    value = aws_security_group.private_host.id
-}
